16603bc3f22e5c1cb8f8cd4bfae1d47c010aad16
Allister Beharry <allister.beharry@gmail.com>
2022-01-08 22:55:14 -0400

    Update SpecSharp.Runtime and MsCorlib.Contracts to build with v4 platform. Made small changes in mscorlib contracts where compiler was giving errors that seem logical...must verify changes are correct. Mscorlib.Contracts builds with Silver compiler.

--- Microsoft.SpecSharp/Runtime/build-runtime.cmd
+++ Microsoft.SpecSharp/Runtime/build-runtime.cmd
@@ -1,3 +1,3 @@
 @echo off
 
-..\..\bin\ssc.exe /p:v4 /debug /t:library /nostdlib /r:mscorlib.dll /keyfile:..\..\Common\InterimKey.snk /r:..\..\System.Compiler.Runtime\bin\System.Compiler.Runtime.dll /out:bin\Microsoft.SpecSharp.Runtime.dll ..\..\Common\Include\Version.cs Classes.ssc Collections.ssc
+..\..\bin\ssc.exe /platform:v4 /debug /t:library /nostdlib /r:mscorlib.dll /keyfile:..\..\Common\InterimKey.snk /r:..\..\System.Compiler.Runtime\bin\System.Compiler.Runtime.dll /out:bin\Microsoft.SpecSharp.Runtime.dll ..\..\Common\Include\Version.cs Classes.ssc Collections.ssc
